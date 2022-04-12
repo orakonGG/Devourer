16f5173b3b035b3cb09d2771448fb5c9867567d0
Lucas Fancher <lukefancher01@gmail.com>
2022-04-12 13:42:05 -0400

    In Class 4/12

--- DevourerGame/Assets/Scripts/PlayerCombat.cs
+++ DevourerGame/Assets/Scripts/PlayerCombat.cs
@@ -2,36 +2,47 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class PlayerCombat : MonoBehaviour
 {
-    private bool meleeActive = false;
-    public BoxCollider2D meleeTrigger;
-    public float damage;
-    
+    // private bool meleeActive = false;
+    // public BoxCollider2D meleeTrigger;
+    // public ContactPoint2D[] contact;
+    // public int GetContacts(contacts); 
+
+
+
+
     // Start is called before the first frame update
-    void Start()
-    {
+    // void Start()
+    // {
         // meleeTrigger = GetComponent<BoxCollider2D>;
-    }
+    // }
 
     // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKeyDown("v") && !meleeActive)
-        {
-            melee();
-        }
-    }
-
-    void melee()
-    {
-        // if (meleeTrigger.IsTouching())
+    // void Update()
+    // {
+        // if (Input.GetButton("Fire1"))
         // {
-             // call damage script
+            
+            
         // }
+    // }
+    
 
-        // wait time
-        // deactivate melee
+    
 
-    }
+    // void melee()
+    // {
+        
+        // {
+            
+        // }
+        // return
+    // }
+
+    // void damage (GameObject character, float meleeDamage)
+    // {
+
+    // }
 }
